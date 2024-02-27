86eb1595f027dc7e21ea7fb1a628e40bb2cbddb2
Derked <nazek8@gmail.com>
2024-02-26 19:54:48 -0500

    catch up on exercises

--- solana-starter/ts/cluster1/spl_metadata.ts
+++ solana-starter/ts/cluster1/spl_metadata.ts
@@ -1,48 +1,70 @@
-import wallet from "../wba-wallet.json"
-import { createUmi } from "@metaplex-foundation/umi-bundle-defaults"
-import { 
-    createMetadataAccountV3, 
-    CreateMetadataAccountV3InstructionAccounts, 
-    CreateMetadataAccountV3InstructionArgs,
-    DataV2Args
+import wallet from "../wba-wallet.json";
+import { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
+import {
+  createMetadataAccountV3,
+  CreateMetadataAccountV3InstructionAccounts,
+  CreateMetadataAccountV3InstructionArgs,
+  DataV2Args,
+  MPL_TOKEN_METADATA_PROGRAM_ID,
 } from "@metaplex-foundation/mpl-token-metadata";
-import { createSignerFromKeypair, signerIdentity, publicKey } from "@metaplex-foundation/umi";
+import {
+  createSignerFromKeypair,
+  signerIdentity,
+  publicKey,
+} from "@metaplex-foundation/umi";
+import { publicKey as publicKeySerializer } from "@metaplex-foundation/umi/serializers";
 
 // Define our Mint address
-const mint = publicKey("<mint address>")
+const mint = publicKey("H4EnJuusXBdPXzk4BvtBrTtzZVUd3xHvzMk1mcgct6DS");
 
 // Create a UMI connection
-const umi = createUmi('https://api.devnet.solana.com');
+const umi = createUmi("https://api.devnet.solana.com");
 const keypair = umi.eddsa.createKeypairFromSecretKey(new Uint8Array(wallet));
 const signer = createSignerFromKeypair(umi, keypair);
 umi.use(signerIdentity(createSignerFromKeypair(umi, keypair)));
 
 (async () => {
-    try {
-        // Start here
-        // let accounts: CreateMetadataAccountV3InstructionAccounts = {
-        //     ???
-        // }
+  try {
+    const metadataSeeds = [
+      Buffer.from("metadata"),
+      publicKeySerializer().serialize(MPL_TOKEN_METADATA_PROGRAM_ID),
+      publicKeySerializer().serialize(mint),
+    ];
+    const metadata = umi.eddsa.findPda(
+      MPL_TOKEN_METADATA_PROGRAM_ID,
+      metadataSeeds
+    );
 
-        // let data: DataV2Args = {
-        //     ???
-        // }
-
-        // let args: CreateMetadataAccountV3InstructionArgs = {
-        //     ???
-        // }
-
-        // let tx = createMetadataAccountV3(
-        //     umi,
-        //     {
-        //         ...accounts,
-        //         ...args
-        //     }
-        // )
-
-        // let result = await tx.sendAndConfirm(umi).then(r => r.signature.toString());
-        // console.log(result);
-    } catch(e) {
-        console.error(`Oops, something went wrong: ${e}`)
-    }
+    // Start here
+    let accounts: CreateMetadataAccountV3InstructionAccounts = {
+      metadata: publicKey(metadata),
+      mint,
+      mintAuthority: signer,
+      updateAuthority: signer,
+    };
+    let data: DataV2Args = {
+      name: "Derked",
+      symbol: "DERK",
+      uri: "",
+      sellerFeeBasisPoints: 0,
+      creators: null,
+      collection: null,
+      uses: null,
+    };
+    let args: CreateMetadataAccountV3InstructionArgs = {
+      data,
+      isMutable: true,
+      collectionDetails: null,
+    };
+    let tx = createMetadataAccountV3(umi, {
+      ...accounts,
+      ...args,
+    });
+    let result = await tx
+      .sendAndConfirm(umi)
+      .then((r) => r.signature.toString());
+    console.log(result);
+  } catch (e) {
+    console.error(`Oops, something went wrong: ${e}`);
+  }
 })();
--- solana-starter/ts/cluster1/spl_transfer.ts
+++ solana-starter/ts/cluster1/spl_transfer.ts
@@ -1,5 +1,11 @@
-import { Commitment, Connection, Keypair, LAMPORTS_PER_SOL, PublicKey } from "@solana/web3.js"
-import wallet from "../wba-wallet.json"
+import {
+  Commitment,
+  Connection,
+  Keypair,
+  LAMPORTS_PER_SOL,
+  PublicKey,
+} from "@solana/web3.js";
+import wallet from "../wba-wallet.json";
 import { getOrCreateAssociatedTokenAccount, transfer } from "@solana/spl-token";
 
 // We're going to import our keypair from the wallet file
@@ -10,19 +16,40 @@ const commitment: Commitment = "confirmed";
 const connection = new Connection("https://api.devnet.solana.com", commitment);
 
 // Mint address
-const mint = new PublicKey("<mint address>");
+const mint = new PublicKey("H4EnJuusXBdPXzk4BvtBrTtzZVUd3xHvzMk1mcgct6DS");
 
 // Recipient address
-const to = new PublicKey("<receiver address>");
+const to = new PublicKey("25xXJNeFNhmM7V2AA3VvEDTrvBnbo4SxcwXL9WhKmcvg");
+
+const token_decimals = 1_000_000n;
 
 (async () => {
-    try {
-        // Get the token account of the fromWallet address, and if it does not exist, create it
-
-        // Get the token account of the toWallet address, and if it does not exist, create it
-
-        // Transfer the new token to the "toTokenAccount" we just created
-    } catch(e) {
-        console.error(`Oops, something went wrong: ${e}`)
-    }
+  try {
+    // Get the token account of the fromWallet address, and if it does not exist, create it
+    const fromTokenAccount = await getOrCreateAssociatedTokenAccount(
+      connection,
+      keypair,
+      mint,
+      keypair.publicKey
+    );
+    // Get the token account of the toWallet address, and if it does not exist, create it
+    const toTokenAccount = await getOrCreateAssociatedTokenAccount(
+      connection,
+      keypair,
+      mint,
+      to
+    );
+    // Transfer the new token to the "toTokenAccount" we just created
+    const tx = await transfer(
+      connection,
+      keypair,
+      fromTokenAccount.address,
+      toTokenAccount.address,
+      keypair,
+      2n * token_decimals
+    );
+    console.log(`Your transfer txid: ${tx}`);
+  } catch (e) {
+    console.error(`Oops, something went wrong: ${e}`);
+  }
 })();
